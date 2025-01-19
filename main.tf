32138eaa54d0ef4796f7134278f6e6312c65d5fd
Bernardo Pires Mesko <bemesko@gmail.com>
2025-01-19 12:40:00 -0300

    Create the Org Configuration repository

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 resource "github_membership" "natalia" {
   username = "natalia-coelho"
 }
+
+resource "github_repository" "github-org" {
+  name        = "github-org"
+  description = "Terraform configuration files for the bernats-homelab organization in GitHub"
+
+  visibility = "public"
+}
---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "config-repo-url" {
+  value = github_repository.github-org.ssh_clone_url
+}
